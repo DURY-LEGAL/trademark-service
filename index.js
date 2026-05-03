489104e1db425602f2d9ebb1765e949ddd12bdb2
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:41:20 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// trademark-service - Trademark service toolkit - End-to-end trademark registration and management. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
